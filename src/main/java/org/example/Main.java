1daf57b0d24f74188e57928097cbfb5ed36a98ce
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-02-26 10:37:11 +0100

    modify main.java file
    add exercise 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 
 public class Main {
     static void main() {
+        //Exercise 1: Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+        System.out.println("Hello");
+        System.out.println("Hans!");
     }
 }
